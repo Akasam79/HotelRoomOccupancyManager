26f03824e6627a088adb2bd5151a9ec85d14960c
Akasam79 <emmachukwuebuka2@gmail.com>
2022-01-13 17:29:23 +0100

    initial commit

---
+++ src/main/java/com/challenge/room/occupancy/HotelRoomOccupancyApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.room.occupancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomOccupancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRoomOccupancyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/room/occupancy/HotelRoomOccupancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.room.occupancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomOccupancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
