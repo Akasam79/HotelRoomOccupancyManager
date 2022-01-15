78074d900a481b72f9bfe3eb3423156074f8ea07
Akasam79 <emmachukwuebuka2@gmail.com>
2022-01-15 23:41:30 +0100

    added parameterizedTest to Service

--- src/test/java/com/challenge/room/occupancy/HotelRoomOccupancyServiceTest.java
+++ src/test/java/com/challenge/room/occupancy/HotelRoomOccupancyServiceTest.java
@@ -2,8 +2,13 @@ package com.challenge.room.occupancy;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -17,86 +22,51 @@ public class HotelRoomOccupancyServiceTest {
 	
 	@Autowired
 	private RoomOccupancyManagerService roomOccupancyManagerService;
-
-	@Test
-	@DisplayName("Test with available rooms - Premium 3 and Economy 3")
-	void test_premium_3_economy_3_shouldReturn_premium_3_economy_3() {
+	
+	@ParameterizedTest
+	@MethodSource("valueInputs")
+	@DisplayName("Test with available rooms")
+	void test_with_Arguments(int availableEconomy, int availablePremium) {
 		
-		// given
 		RoomsAvailable roomsAvailable = RoomsAvailable.builder()
-				.availableEconomyRoom(3)
-				.availablePremiumRoom(3)
+				.availableEconomyRoom(availableEconomy)
+				.availablePremiumRoom(availablePremium)
 				.build();
 		
-		/// when
 		RoomsUsageAndRevenue usageAndRevenue = roomOccupancyManagerService.calculateRoomUsage(roomsAvailable);
 
-		//then
-		then(usageAndRevenue.getEconomyGuestSum()).isEqualTo(3);
-		then(usageAndRevenue.getEconomyIncomeSum()).isEqualTo("EUR 167.99");
-		then(usageAndRevenue.getPremiumGuestSum()).isEqualTo(3);
-		then(usageAndRevenue.getPremiumIncomeSum()).isEqualTo("EUR 738.0");
+		if(availableEconomy == 3) {
+			then (usageAndRevenue.getEconomyGuestSum()).isEqualTo(3);
+			then(usageAndRevenue.getEconomyIncomeSum()).isEqualTo("EUR 167.99");
+			then(usageAndRevenue.getPremiumGuestSum()).isEqualTo(3);
+			then(usageAndRevenue.getPremiumIncomeSum()).isEqualTo("EUR 738.0");
+		}else if(availableEconomy == 5) {
+			then(usageAndRevenue.getEconomyGuestSum()).isEqualTo(4);
+			then(usageAndRevenue.getEconomyIncomeSum()).isEqualTo("EUR 189.99");			
+			then(usageAndRevenue.getPremiumGuestSum()).isEqualTo(6);
+			then(usageAndRevenue.getPremiumIncomeSum()).isEqualTo("EUR 1054.0");
+		}else if(availableEconomy == 7) {
+			then(usageAndRevenue.getEconomyGuestSum()).isEqualTo(4);
+			then(usageAndRevenue.getEconomyIncomeSum()).isEqualTo("EUR 189.99");
+			then(usageAndRevenue.getPremiumGuestSum()).isEqualTo(2);
+			then(usageAndRevenue.getPremiumIncomeSum()).isEqualTo("EUR 583.0");
+		}else {
+			then(usageAndRevenue.getEconomyGuestSum()).isEqualTo(1);
+			then(usageAndRevenue.getEconomyIncomeSum()).isEqualTo("EUR 45.0");
+			then(usageAndRevenue.getPremiumGuestSum()).isEqualTo(7);
+			then(usageAndRevenue.getPremiumIncomeSum()).isNotEqualTo("EUR 1153.0");
+		}
+		
 	}
 	
-	@Test
-	@DisplayName("Test with available rooms - Premium 7 and Economy 5")
-	void test_premium_7_economy_5_shouldReturn_premium_6_economy_4() {
-		
-		// given
-		RoomsAvailable roomsAvailable = RoomsAvailable.builder()
-				.availableEconomyRoom(5)
-				.availablePremiumRoom(7)
-				.build();
-		
-		/// when
-		RoomsUsageAndRevenue usageAndRevenue = roomOccupancyManagerService.calculateRoomUsage(roomsAvailable);
+	private static Stream<Arguments> valueInputs(){
+		return Stream.of(
+				Arguments.of(3, 3),
+				Arguments.of(5, 7),
+				Arguments.of(7, 2),
+				Arguments.of(1, 7)
+				);
+	}
 
-		//then
-		then(usageAndRevenue.getEconomyGuestSum()).isEqualTo(4);
-		then(usageAndRevenue.getEconomyIncomeSum()).isEqualTo("EUR 189.99");
-		then(usageAndRevenue.getPremiumGuestSum()).isEqualTo(6);
-		then(usageAndRevenue.getPremiumIncomeSum()).isEqualTo("EUR 1054.0");
-	}
-	
-	@Test
-	@DisplayName("Test with available rooms - Premium 2 and Economy 7")
-	void test_premium_2_economy_7_shouldReturn_premium_2_economy_4() {
-		
-		// given
-		RoomsAvailable roomsAvailable = RoomsAvailable.builder()
-				.availableEconomyRoom(7)
-				.availablePremiumRoom(2)
-				.build();
-		
-		/// when
-		RoomsUsageAndRevenue usageAndRevenue = roomOccupancyManagerService.calculateRoomUsage(roomsAvailable);
-
-		//then
-		then(usageAndRevenue.getEconomyGuestSum()).isEqualTo(4);
-		then(usageAndRevenue.getEconomyIncomeSum()).isEqualTo("EUR 189.99");
-		then(usageAndRevenue.getPremiumGuestSum()).isEqualTo(2);
-		then(usageAndRevenue.getPremiumIncomeSum()).isEqualTo("EUR 583.0");
-	}
-	
-	
-	@Test
-	@DisplayName("Test with available rooms - Premium 7 and Economy 1")
-	void test_premium_7_economy_1_shouldReturn_premium_7_economy_1() {
-		
-		// given
-		RoomsAvailable roomsAvailable = RoomsAvailable.builder()
-				.availableEconomyRoom(1)
-				.availablePremiumRoom(7)
-				.build();
-		
-		/// when
-		RoomsUsageAndRevenue usageAndRevenue = roomOccupancyManagerService.calculateRoomUsage(roomsAvailable);
-
-		//then
-		then(usageAndRevenue.getEconomyGuestSum()).isEqualTo(1);
-		then(usageAndRevenue.getEconomyIncomeSum()).isEqualTo("EUR 45.0");
-		then(usageAndRevenue.getPremiumGuestSum()).isEqualTo(7);
-		then(usageAndRevenue.getPremiumIncomeSum()).isNotEqualTo("EUR 1153.0");
-	}
 
 }
